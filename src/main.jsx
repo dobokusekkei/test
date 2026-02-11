1c9220d87895592f8a832918c133e9ae74d72b01
dobokusekkei <take10140715@gmail.com>
2026-02-11 23:54:43 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// ↓ ファイル名が app.jsx (小文字) なら ./app.jsx、App.jsx (大文字) なら ./App.jsx にする
+import App from './app.jsx' 
+import './index.css' // もしCSSファイルを作っていたらimport。なければこの行は削除。
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
